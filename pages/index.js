47a792d347366c775e2c9ec589c8c0d66d73b93e
Paulo Osawa <122981510+PauloOsawa@users.noreply.github.com>
2023-02-07 11:21:35 -0300

    Construção da tela inicial

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,55 @@
+import { useState } from 'react'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import css from '@/styles/Home.module.css'
 
 export default function Home() {
+
+  const [cldvTxt, setCldvTxt] = useState(css.dvtxt)
+  const frmView = !(cldvTxt === css.dvtxt)
+
+  // ---------------------------------------------------
+  const iniciar = () => console.log('PROJETO EM CONSTRUÇÃO',  );
+
   return (
     <>
       <Head>
         <title>Time is Money</title>
         <meta name="description" content="App de entretenimento com ênfase em cálculos de data e finanças" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="author" content="Paulo Osawa"/>
+        <meta name="robots" content="noindex, nofollow" />
+        <meta name="author" content="Paulo Osawa" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1>App em Desenvolvimento</h1>
+      <main className={css.main}>
+        <div className='metalbox'> <h1 className='tita'>Time Is Money</h1> </div>
+        <h1>Time Is Money</h1>
+
+        <div className={cldvTxt} >
+          <p>
+            Um projeto divertido e interativo, que realiza diversos cálculos financeiros em
+            <br />conjunto com datas, além de curiosidades diversas, com várias opções de ecolha!
+          </p>
+          <h3>Funciona Basicamente Assim:</h3>
+          <p>
+            Você insere diversos tipos de gastos, lucros, e até poupanças!!
+            <br />Depois escolhe uma data futura, ou um saldo desejado e verá todo o saldo credor ou devedor!
+          </p>
+          <p>
+            Pra começar a brincadeira, perguntaremos informações básicas,
+            <br />para calcular curiosidades além de uma simples planilha!
+          </p>
+          <p>
+            OBS.: <b>Não armazenamos cookies nem quaisquer informações</b> suas OK?
+            <br />Você pode inventar tudo, mas pra conferir depois será mais difícil!
+          </p>
+
+          <div className={css.dvinit}>
+            <h2>VAMOS COMEÇAR???</h2>
+            <button className='btncor' onClick={iniciar}>SIM</button>
+          </div>
+        </div>
+
+
       </main>
     </>
   )
