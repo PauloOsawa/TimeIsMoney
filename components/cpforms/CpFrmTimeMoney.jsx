624a4e8eea38f7ab2f7fe678a84389730c61b462
Paulo Osawa <122981510+PauloOsawa@users.noreply.github.com>
2023-02-09 03:40:20 -0300

    Adicionando hook useTimeMoney

--- components/cpforms/CpFrmTimeMoney.jsx
+++ components/cpforms/CpFrmTimeMoney.jsx
@@ -1,9 +1,14 @@
 import { useRef } from "react";
-
+import useTimeMoney from "@/hooks/useTimeMoney";
 import css from "@/styles/CpFrmTimeMoney.module.css";
 import CpSelsDate from "./CpFrmSelsDate";
+import CpTbMoney from "./CpTbMoney";
+import CpFrmInptPrice from "./CpFrmInptPrice";
 
 export default function CpFrmTimeMoney({ hoje, hideFrm }){
+
+  const { dados, setbirth, addMoney, setgastos, setDados, calcAll } = useTimeMoney(hoje);
+  const sthj = dados.dtHoje.toISOString().slice(0, 10);
 
   const fldact = useRef();
   //#region ---------------------------- HTML ------------
@@ -30,14 +35,77 @@ export default function CpFrmTimeMoney({ hoje, hideFrm }){
     if (toHide) { el.classList.add(css.hidenb); return; }
     el.classList.remove(css.hidenb);
   }
+
+  const showDvCalc = (e) => {
+    const idx = e.target.value === 'data' ? 0 : 1;
+    [css.dvDt, css.dvsaldo].forEach((cl, i) => {
+      const toHide = (idx !== i);
+      viewElm(cl, toHide);
+    })
+  }
   //#endregion ---------------------
-  
+
+  //#region -------------------------- DATA --------------
+  const checkExistKname = (tipo, nome) => {
+    const ob = (tipo in dados) ? dados[tipo].find(obj => obj.nome === nome) : false;
+    return !!ob;
+  }
+
+  const endCalc = (e) => {
+    let ob;
+    if (typeof e === 'number') {
+      console.log('calcular A DATA val=', e);
+      ob = {saldoCalc: e}
+      // calcAll({saldoCalc: e});
+      // return;
+    } else {
+      const inpdt = e.target.previousElementSibling;
+      if(inpdt.nodeName !== 'INPUT' || !inpdt?.validity?.valid){ console.log('invalido',  ); return; }
+      const strdt = inpdt.value  + ' 00:00';
+      ob = {dtCalc: new Date(strdt)}
+      // setDados({...dados, dtCalc: new Date(strdt)});
+      // calcAll({dtCalc: new Date(strdt)});
+      e.preventDefault();
+    }
+    calcAll(ob);
+    viewElm(css.dvfinal);
+  }
+  //#endregion ---------------------
+
+  // ---------------------------------------- FN FLDS -----------
+  const setaLucros = (l) => {
+    console.log('setaLucros',  );
+    setDados({...dados, lucros: l});
+  }
+  //#region ----------------------------------- CAR ---------
+  const setIpva = (ipva, compPrice) => {
+    if (!checkExistKname('gastos', 'IPVA')) {
+      addMoney('gastos', ['IPVA','Anual', ipva ]);
+    }
+    if (checkExistKname('gastos', 'Combustível')) {
+      goFld(fldact.current.nextElementSibling);
+    }
+  }
+
+  const getValGas = (valgas, compPrice) => {
+    addMoney('gastos', ['Combustível','Mensal', valgas ]);
+    compPrice.classList.add(css.hidenb);
+  }
+
+  const showdvcar = (e) => {
+    e.preventDefault()
+    const dvask = e.target.parentNode;
+    dvask.classList.add(css.hidenb);
+    dvask.nextElementSibling.classList.remove(css.hidenb);
+  }
+  //#endregion ---------
+
   //#region ------------------------------------ NIVER ----
   const setDtbirth = (d) => {
     if (!d || !d.length || d.length != 3) { return; }
     console.log('d = ', d );
-    // setbirth(d);
-    // showNextFld();
+    setbirth(d);
+    showNextFld();
   }
   //#endregion ---------------------------------------
 
@@ -51,12 +119,65 @@ export default function CpFrmTimeMoney({ hoje, hideFrm }){
         <CpSelsDate setDate={setDtbirth}/>
       </fieldset>
 
+      <fieldset name='fldcar' className={css.fld}>
+        <legend>Veículos</legend>
+        <div className={css.dvcar}>
+          <h3>Você Possui Algum Veículo?</h3>
+          <button onClick={showNextFld} className="btncor">NÃO</button>
+          <button onClick={showdvcar} className="btncor">SIM</button>
+        </div>
+
+        <div className={`${css.dvcar} ${css.hidenb}`}>
+          <CpFrmInptPrice title={'Quanto você gasta de combustível por mês?'} getVals={getValGas} />
+          <CpFrmInptPrice title={'Quanto você gasta de IPVA por ano?'} getVals={setIpva} />
+        </div>
+      </fieldset>
+
+      <fieldset name='fldgastos' className={css.fld}>
+        <legend>Despesas</legend>
+        <CpTbMoney title={'GASTOS'} valores={dados?.gastos ?? []} setaVals={setgastos} fecha={showNextFld} />
+      </fieldset>
+
+      <fieldset name='fldlucros' className={css.fld}>
+        <legend>Lucros</legend>
+        <CpTbMoney title={'LUCROS'} valores={dados?.lucros ?? []} setaVals={setaLucros} voltar={backfld} fecha={showNextFld} />
+      </fieldset>
+
+      <fieldset name='fldcalc' className={css.fld}>
+        <legend>Cálculo</legend>
+        <h3>Escolha uma Data para saber um Saldo, ou Vice-Versa!</h3>
+        <div>
+          <label>Calcular POR:</label>
+          <input type="radio" name='radCalc' value={'data'} onChange={showDvCalc} />DATA
+          <input type="radio" name='radCalc' value={'saldo'} onChange={showDvCalc} />SALDO
+        </div>
+
+        <div className={`${css.dvDt} ${css.hidenb}`}>
+          {/* <h4>Insira uma DATA e saiba QUANTO irá conseguir!</h4> */}
+          <input type="date" defaultValue={sthj} min={sthj} max={"2060-11-30"} required />
+          <button className="btncor" onClick={endCalc}>CALCULAR</button>
+        </div>
+
+        <div className={`${css.dvsaldo} ${css.hidenb}`}>
+          {/* <CpFrmInptPrice title={'Insira um VALOR e saiba QUANDO irá conseguir!'} getVals={endCalc} /> */}
+          <CpFrmInptPrice getVals={endCalc} action={'CALCULAR'}/>
+        </div>
+
+      </fieldset>
+
       <div className={css.lbtns}>
         <button onClick={backfld}>VOLTAR</button>
         <input type="reset" value="CANCELAR" />
       </div>
 
     </form>
+
+    <div className={`${css.dvfinal} ${css.hidenb}`}>
+      <h3>RESULTS</h3>
+      {!!dados?.result?.map((r, i) => (
+        <p key={i}>{r}</p>
+      ))}
+    </div>
     </>
   )
 }
---
+++ hooks/useTimeMoney.js
@@ -0,0 +1,191 @@
+import { useState } from "react";
+
+export default function useTimeMoney(hoje) {
+
+  const dthj = new Date(hoje);
+  const arDtHj = [dthj.getFullYear(), dthj.getMonth(), dthj.getDate()]
+
+  const userdata = {
+    dtHoje: dthj ?? null, dtNasc: null, idade: 0,
+  }
+
+  const [dados, setDados] = useState({ ...userdata});
+  const mesesMaxDays = [ 0, 2, 4, 6, 7, 9, 11 ]
+  const diasSemana = [ 'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado' ]
+  const meses = [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho',
+    'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+  ]
+
+  //#region ======================================== DATE HELPERS
+  const getDiasNoMes = (mes, ano) => {
+    if (mes === 1) {  return (ano % 4 === 0) ? 29 : 28; }
+    return mesesMaxDays.includes(mes) ? 31 : 30;
+  }
+
+  const getNumDays = (ms) => {
+    const totDays = ms / (1000 * 60 * 60 * 24);
+    // console.log('totDays = ', totDays );
+    return (totDays);
+  }
+  //#endregion =======================================
+
+  const setgastos = (gastos) => { setDados({...dados, gastos: gastos}); }
+
+  const addMoney = (tipo, arMoney) => {
+    const obj = { nome: arMoney[0], periodo: arMoney[1], valor: arMoney[2] }
+    let arDados = (tipo in dados) ? [...dados[tipo], obj]: [obj];
+    setDados({...dados, [tipo]: arDados});
+  }
+
+  //#region ----------------------------- FIELD NIVER
+  const setaIdade = (arNasc) => {
+    const [anoNasc, mesNasc, ddNasc] = arNasc;
+    let lastAnoNiver = arDtHj[0];
+    const lastniver = new Date(lastAnoNiver, mesNasc, ddNasc);
+    const fezNiverNoAno = dthj > lastniver;
+    const nextniver = !fezNiverNoAno ? new Date(arDtHj[0], mesNasc, ddNasc) : new Date((arDtHj[0]+1), mesNasc, ddNasc);
+    const diasRestPniver = getNumDays(nextniver - dthj);
+    if(!fezNiverNoAno){ lastAnoNiver--;  lastniver.setFullYear(lastAnoNiver); }
+    const diasPosNiver = getNumDays(dthj - lastniver);
+
+    const objIdade = {
+      idade: lastAnoNiver - anoNasc,
+      diasPosNiver: diasPosNiver.toFixed(1),
+      diasRestPniver: diasRestPniver.toFixed(1)
+    }
+    return objIdade;
+  }
+  /* ------------------------------- */
+  const setbirth = (d) => {
+    const dtNasc = new Date(d[0], d[1], d[2], 0);
+    if (!(!dados?.dtNasc || dados.dtNasc?.getTime() !== dtNasc.getTime())) { return; }
+    const diasVida = parseInt(getNumDays(dthj - dtNasc));
+    const objNiver = {  arNasc: d, dtNasc: dtNasc, diasVida: diasVida }
+    const objIdade = setaIdade(d);
+    setDados({...dados, ...objNiver, ...objIdade});
+  }
+  //#endregion ----
+
+  //#region ======================================== CALC FIM
+  const setFinalResults = () => {
+    console.log('setFinalResults',  );
+    const results = []
+    results.push('linha a');
+    results.push('linha b');
+    // setDados({...dados, result:[...results]})
+
+  }
+
+  const calcByDt = (dtCalc, saldos) => {
+    console.log('calcByDt',  );
+    let total = 0;
+    const arDtCalc = [dtCalc.getFullYear(), dtCalc.getMonth(), dtCalc.getDate()]
+    const {Anual, Mensal, Diário } = saldos.totalSaldos;
+
+    const totDias = parseInt(getNumDays(dtCalc - dthj));
+    const valDias = Diário*totDias;
+    console.log('totDias = ', totDias, valDias );
+    total += valDias;
+
+    const difMesesInAno = arDtCalc[1] >= arDtHj[1] ? arDtCalc[1] - arDtHj[1] : 12 - (arDtHj[1] - arDtCalc[1]);
+    const totAnos = parseInt(totDias/365);
+    const totMeses = totAnos*12 + difMesesInAno;
+    const valMeses = Mensal*totMeses;
+    console.log('totMeses = ', totMeses, valMeses );
+    total += valMeses;
+
+    const difAnos = arDtCalc[0] - arDtHj[0];
+    const valAno = Anual*difAnos;
+    console.log('difAnos = ', difAnos, valAno );
+    total += valAno;
+  }
+
+  const calcByVal = (val, saldos) => {
+    console.log('calcByVal',  );
+    const {Anual, Mensal, Diário } = saldos.totalSaldos;
+    let [ano, mes, dia] = [...arDtHj]
+    const getSumDiasMes = (mes, ano) => Diário*getDiasNoMes(mes, ano);
+    let totalDias = 0;
+    let totalMeses = 0;
+    let totalAnos = 0;
+    let totalVal = 0;
+    const retorna = () => {
+      return { totais:[totalAnos, totalMeses, totalDias], arData: [ano + totalAnos, mes, dia ]  }
+    }
+
+    let somaDiasInMes = getSumDiasMes(mes, ano);
+    if (somaDiasInMes >= val){totalDias = parseInt(val/Diário); retorna(); }
+
+    let somaMes = Mensal + somaDiasInMes;
+    if (somaMes >= val) { totalMeses++; totalDias += getDiasNoMes(mes, ano); retorna(); }
+
+    const somaInAno = (Mensal*12) + (Diário*365);
+    if (somaInAno >= val) {
+      totalMeses = parseInt(val/somaMes);
+      totalDias = (totalMeses*30) + (totalMeses/2) + (totalMeses % 30);
+      retorna();
+    }
+    const somaAno = Anual + (Mensal*12) + (Diário*365);
+    const mediaMes = somaAno/12;
+
+    if (somaAno >= val) { totalMeses++; totalDias = getDiasNoMes(mes, ano); }
+
+    if (mediaMes < 0) { retorna(); }
+
+    if (val < somaDiasInMes || val < somaMes) {
+      totalDias = val < somaDiasInMes ? parseInt(val/Diário) : tdias;
+      totalMeses = val < somaDiasInMes ? 0 : 1;
+      retorna();
+    }
+
+    totalVal = somaMes;
+    totalMeses++;
+  }
+
+  const somaVals = (tipo) => {
+    const obval = {Anual: 0, Mensal: 0, Diário: 0}
+    if(!tipo in dados){ console.log('sem', tipo ); return obval; }
+    dados[tipo].forEach(val => {  obval[val.periodo] += val.valor; });
+    return obval;
+  }
+
+  const setSaldos = () => {
+    // const obGastos = {Anual: 0, Mensal: 0, Diário: 0}
+    const saldos = { totalLucro: somaVals('lucros'), totalGasto: somaVals('gastos') }
+    // const obLucros = somaVals('lucros');
+    // const obGastos = somaVals('gastos');
+    const obSaldos = {}
+    for(let k in saldos.totalLucro){  obSaldos[k] = saldos.totalLucro[k] - saldos.totalGasto[k]; }
+    saldos.totalSaldos = obSaldos;
+
+    const somaDiasInMes = obSaldos.Diário*30;
+    const somaMensalDias = obSaldos.Mensal + somaDiasInMes;
+    const somaMesesInAno = somaMensalDias*12;
+    const somaAnoMeses = obSaldos.Anual + somaMesesInAno;
+    const totalSomas = { somaAnual: somaAnoMeses, somaMensalDias: somaMensalDias, somaDiasInMes: somaDiasInMes }
+
+    return {...saldos, ...totalSomas};
+    // setValsDts(obj, saldos);
+    // return {totalLucro: obLucros, totalGasto: obGastos, totalSaldos: obSaldos }
+    // setDados({...dados, totalLucro: {...obLucros}, totalGasto: {...obGastos}, totalSaldos: {...obSaldos}});
+  }
+
+  const calcAll = (obj) => {
+    // setDados({...dados, ...obj});
+    const saldos = setSaldos();
+    if(!!obj?.dtCalc){
+      calcByDt(obj.dtCalc, saldos );
+    } else {
+      calcByVal(obj.saldoCalc, saldos );
+    }
+    // setCurious();
+    setFinalResults();
+
+  }
+  //#endregion =======================================
+
+  return {
+    dados, setbirth, addMoney, setgastos, setDados, calcAll
+  }
+}
